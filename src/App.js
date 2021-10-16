a378e2755ebbfdfae87825b19c23c93e8b5c3db6
shota4869 <shooochan.t@gmail.com>
2021-10-16 16:01:50 +0900

    Lesson9

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World. boiler
-        </p>
-        <a
-  
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div><h1>Hello World. boiler</h1></div>
 }
 
 export default App;
