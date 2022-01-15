eb902c52c545774cf294d33398147f0771f735b5
shota4869 <shooochan.t@gmail.com>
2022-01-16 00:13:03 +0900

    jsxの使い方について

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import React,{ Component } from "react";
 
-function App() {
-  return <h1>Hello World!!</h1>
+// class App extends Component {
+//   render(){
+//     const greeting = "Hi,Shota";
+//     const dom = <h1 className ="foo">{greeting}</h1>;
+//     HTMLのClassはclassNameとする
+//     return dom;
+//   }
+// }
+
+// class App extends Component {
+//   render(){
+//     return <input type="text" onChange={() => {console.log("I am clicked")}}/>;
+//   }
+// }
+
+class App extends Component {
+  render(){
+    return(
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked")}}/>;
+      </React.Fragment>
+    ) 
+  }
 }
 
 export default App;
