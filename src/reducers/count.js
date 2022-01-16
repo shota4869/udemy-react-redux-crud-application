ad5eae08ad7e594227d636eec97b742ddecda94b
shota4869 <shooochan.t@gmail.com>
2022-01-16 22:33:06 +0900

    modify_count.js

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,8 +1,8 @@
-import { INCREMENT, DECREMENT } from "../actions"
+import { INCREMENT, DECREMENT } from '../actions'
 
 const initialState = { value: 0 }
 
-export default (state = initialState,action) =>{
+const switchEvents = (state = initialState, action ) => { 
     switch(action.type){
         case INCREMENT:
             return { value: state.value + 1}
@@ -12,3 +12,5 @@ export default (state = initialState,action) =>{
             return state
     }
 } 
+
+export default switchEvents;
