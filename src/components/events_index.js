379ce27fb02ecff2970cdb1e261f8b153517588e
高瀬翔太 <takaseshota@takaseshoutanoiMac.local>
2022-01-30 15:03:01 +0900

    implement event new component

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import _ from 'lodash'
+import { Link } from 'react-router-dom'
 
 import { readEvents } from '../actions'
 
@@ -42,6 +43,8 @@ class EventsIndex extends Component{
             {this.renderEvents()}
           </tbody>
         </table>
+        
+        <Link to="/events/new">New Event</Link>
       </React.Fragment>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,18 +3,26 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux' //storeの内容をアプリケーションの全コンポーネントに渡す機能
 import thunk from 'redux-thunk'
+import { BrowserRouter , Route , Routes } from 'react-router-dom'
 
 import './index.css';
 import reducer from './reducers'
 
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new'
 import reportWebVitals from './reportWebVitals';
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <EventsIndex />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/events/new" element={<EventsNew/>}/>
+        {/* react-router-domV6ではコンポーネント属性がelement属性になっている */}
+        <Route exact path="" element={<EventsIndex/>} />
+      </Routes>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
